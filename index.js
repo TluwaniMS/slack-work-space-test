15fdb1aaa2208edae43e945140dee1b12419d0f5
tluwaniMS <tluwani2020@gmail.com>
2022-01-27 21:03:41 +0200

    adding-initial-configs-for-bolt-app

---
+++ index.js
@@ -0,0 +1,16 @@
+const { App } = require("@slack/bolt");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const app = new App({
+  token: process.env.SLACK_BOT_TOKEN,
+  signingSecret: process.env.SLACK_SIGNING_SECRET
+});
+
+(async () => {
+  // Start your app
+  await app.start(process.env.PORT || 3000);
+
+  console.log("⚡️ Bolt app is running!");
+})();
